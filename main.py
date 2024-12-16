ff4f22d0bceb0965cb59704590287d58fc286470
HiralChauhan21 <vishal.chauhan.home@gmail.com>
2024-12-16 16:08:52 +0530

    commit

---
+++ main.py
@@ -0,0 +1,9 @@
+setc1 = {"green", "blue"}
+setc2 = {"blue", "yellow"}
+print("original sets: ")
+print(setc1)
+print(setc2)
+
+setc = setc1.union(setc2)
+print("\nUnion of the above sets: ")
+print(setc)
